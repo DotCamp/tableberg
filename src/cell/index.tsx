c36453725657b5c4e9725b58c22a0cff394060f8
Zahin Azmayeen <zahin06@gmail.com>
2023-09-13 07:16:25 +0600

    cell: add content align support
    Right now, all child elements will have the same alignment.
    We have to consider flexbox or grid if
    we want elements to have individual alignments.

--- src/cell/index.tsx
+++ src/cell/index.tsx
@@ -1,16 +1,27 @@
 import { BlockEditProps, registerBlockType } from "@wordpress/blocks";
-import { store as blockEditorStore } from "@wordpress/block-editor";
+import {
+    // @ts-ignore
+    AlignmentControl,
+    BlockControls,
+    store as blockEditorStore,
+} from "@wordpress/block-editor";
 import {
     useBlockProps,
     useInnerBlocksProps,
     InnerBlocks,
 } from "@wordpress/block-editor";
 import { useSelect } from "@wordpress/data";
+import { alignLeft, alignRight, alignCenter } from "@wordpress/icons";
 
 import "./style.scss";
 import "./editor.scss";
 
 import metadata from "./block.json";
+import classNames from "classnames";
+
+interface TablebergCellBlockAttrs {
+    align: string;
+}
 
 const ALLOWED_BLOCKS = [
     "core/image",
@@ -22,21 +33,63 @@ const ALLOWED_BLOCKS = [
     "core/paragraph",
 ];
 
-function edit({ clientId }: BlockEditProps<{}>) {
-    const blockProps = useBlockProps();
-
+function edit({
+    clientId,
+    attributes,
+    setAttributes,
+}: BlockEditProps<TablebergCellBlockAttrs>) {
     const hasInnerBlocks = useSelect(
         (select) =>
             (select(blockEditorStore) as any).getBlocks(clientId).length > 0,
         [clientId]
     );
 
+    const { align } = attributes;
+
+    const className = classNames({
+        [`has-text-align-${align}`]: align,
+    });
+
+    const blockProps = useBlockProps({ className });
+
+    // @ts-ignore
     const innerBlocksProps = useInnerBlocksProps(blockProps, {
         allowedBlocks: ALLOWED_BLOCKS,
         template: [["core/paragraph"]],
     });
 
-    return <td {...innerBlocksProps} />;
+    return (
+        <>
+            <td {...innerBlocksProps} />
+            {/* @ts-ignore */}
+            <BlockControls group="block">
+                <AlignmentControl
+                    value={align}
+                    onChange={(nextAlign: string) => {
+                        setAttributes({ align: nextAlign });
+                    }}
+                    label="Align Content"
+                    alignmentControls={[
+                        {
+                            icon: alignLeft,
+                            title: "Align content left",
+                            align: "left",
+                        },
+                        {
+                            icon: alignCenter,
+                            title: "Align content center",
+                            align: "center",
+                        },
+                        {
+                            icon: alignRight,
+                            title: "Align content right",
+                            align: "right",
+                        },
+                    ]}
+                />
+            </BlockControls>
+        </>
+    );
 }
 
 function save() {
@@ -48,7 +101,11 @@ function save() {
 registerBlockType(metadata.name, {
     title: metadata.title,
     category: metadata.category,
-    attributes: {},
+    attributes: {
+        align: {
+            type: "string",
+        },
+    },
     example: {},
     edit,
     save,
