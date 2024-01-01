e5f0bd3a54d984a3559c07be6a02598ddb7c3c58
Zahin Azmayeen <zahin06@gmail.com>
2024-01-02 00:46:25 +0600

    remove build dir and add to gitignore

--- build/cell/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-primitives'), 'version' => '660a521e9daeba32628a');
--- build/image/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '29625a18d83712d83158');
