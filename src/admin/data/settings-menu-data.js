698aa43f024bb8411921216ff43543759b74a236
Zahin Azmayeen <zahin06@gmail.com>
2024-01-05 03:10:35 +0600

    fix change documentation url

--- src/admin/data/settings-menu-data.js
+++ src/admin/data/settings-menu-data.js
@@ -3,6 +3,6 @@ import { __ } from "@wordpress/i18n";
 export const welcomePageAssets = {
     proBuyUrl: "https://ultimateblocks.com/pricing/",
     youtubeVideoId: "TKsL_bUVCTU",
-    documentsUrl: "https://tableberg.com/docs/",
+    documentsUrl: "https://tableberg.com/getting-started/",
     supportUrl: "https://tableberg.com/contact/",
 };
