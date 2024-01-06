cb2f7094f4aaca7e61c3e2b8e909d84e09898675
Zahin Azmayeen <zahin06@gmail.com>
2024-01-06 17:43:05 +0600

    remove extraneous @ts-ignore

--- src/controls.tsx
+++ src/controls.tsx
@@ -3,7 +3,6 @@
  */
 import { __ } from "@wordpress/i18n";
 import { justifyLeft, justifyCenter, justifyRight } from "@wordpress/icons";
-//@ts-ignore
 import {
     InspectorControls,
     // @ts-ignore
