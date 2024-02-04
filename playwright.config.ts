f3b0d653d48fbb9344123797cdc3433d9d7048ae
Zahin Azmayeen <zahin06@gmail.com>
2024-02-04 21:52:02 +0600

    change retries and video options
    remove comments

--- playwright.config.ts
+++ playwright.config.ts
@@ -5,25 +5,16 @@ dotenv.config();
 
 export default defineConfig({
     testDir: "./tests",
-    /* Fail the build on CI if you accidentally left test.only in the source code. */
     forbidOnly: !!process.env.CI,
-    /* Retry on CI only */
-    retries: process.env.CI ? 2 : 0,
-    /* Opt out of parallel tests on CI. */
+    retries: 1,
     workers: process.env.CI ? 1 : undefined,
-    /* Reporter to use. See https://playwright.dev/docs/test-reporters */
     reporter: "html",
     fullyParallel: true,
-    /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
     use: {
-        /* Base URL to use in actions like `await page.goto("/")`. */
-        baseURL: process.env.baseURL ?? "http://127.0.0.1",
-
-        /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+        baseURL: process.env.BASE_URL ?? "http://127.0.0.1",
         trace: "on-first-retry",
+        video: "on-first-retry"
     },
-    // globalSetup: require.resolve("./global-setup"),
-    /* Configure projects for major browsers */
     projects: [
         {
             name: "auth setup",
