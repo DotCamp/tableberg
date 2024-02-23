f7fc67b8b35ba6c2cbf861ca324bc2c8f2535802
Sakibur Rahman Khan <sakib.saad.khan@gmail.com>
2024-02-23 17:21:49 +0600

    clear logging

--- includes/assets/js/PreviewDeviceChangeObserver.js
+++ includes/assets/js/PreviewDeviceChangeObserver.js
@@ -11,12 +11,10 @@
         );
         lastDevice = previewEl.className.match(/is-(\w*)-preview/)[1];
         const previewObserver = new MutationObserver((mutationList) => {
-            console.log(mutationList);
             for (let mutation of mutationList) {
                 if (mutation.attributeName === "class") {
                     const currentPreview =
                         mutation.target.className.match(/is-(\w*)-preview/)[1];
-                        console.log("In:", currentPreview);
                     if (!currentPreview) {
                         return;
                     }
