039eb54d70961af53dad64880e296e0292d554fa
Zahin Azmayeen <zahin06@gmail.com>
2024-03-03 23:35:47 +0600

    version bump and update changelog

--- tableberg.php
+++ tableberg.php
@@ -4,7 +4,7 @@
  * Plugin Name:       Tableberg
  * Plugin URI:        https://tableberg.com/
  * Description:       Table Block by Tableberg - Create Better Tables With Block Editor
- * Version:           0.3.2
+ * Version:           0.3.3
  * Requires at least: 6.1
  * Requires PHP:      7.0
  * Author:            Tableberg
