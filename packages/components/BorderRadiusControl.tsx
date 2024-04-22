9a08f886c43140c5ce5f0e101d4827d4dc5a84ea
Saad <sakib.saad.khan@gmail.com>
2024-04-22 16:11:27 +0600

    added border radius control

---
+++ packages/components/BorderRadiusControl.tsx
@@ -0,0 +1,54 @@
+/**
+ * WordPress Dependencies
+ */
+import { isEmpty } from "lodash";
+import { __ } from "@wordpress/i18n";
+import {
+    useBlockEditContext,
+    __experimentalBorderRadiusControl as RadiusControl,
+} from "@wordpress/block-editor";
+import {
+    __experimentalToolsPanelItem as ToolsPanelItem,
+} from "@wordpress/components";
+
+interface BorderRadiusControlPropTypes {
+    label: string;
+    value: any;
+    onChange: (newBorder: any) => any;
+    resetAllFilter?: () => any;
+    onDeselect: () => any;
+    isShownByDefault?: boolean;
+}
+
+function BorderRadiusControl({
+    label,
+    isShownByDefault = true,
+    value,
+    onChange = () => {},
+    resetAllFilter,
+    onDeselect = () => {},
+}: BorderRadiusControlPropTypes) {
+    const { clientId } = useBlockEditContext();
+
+    if (!resetAllFilter) {
+        resetAllFilter = onDeselect;
+    }
+
+    return (
+        <ToolsPanelItem
+            panelId={clientId}
+            isShownByDefault={isShownByDefault}
+            resetAllFilter={resetAllFilter}
+            hasValue={() => !isEmpty(value)}
+            label={label}
+            onDeselect={onDeselect}
+        >
+            <RadiusControl
+                onChange={onChange}
+                values={value}
+            />
+        </ToolsPanelItem>
+    );
+}
+
+export default BorderRadiusControl;
--- packages/components/index.ts
+++ packages/components/index.ts
@@ -1,4 +1,5 @@
 export { default as BorderControl } from "./BorderControl";
+export { default as BorderRadiusControl } from "./BorderRadiusControl";
 export { default as ColorControl } from "./ColorControl";
 export { default as ColorPickerDropdown } from "./ColorPickerDropdown";
 export { default as SpacingControl } from "./SpacingControl";
