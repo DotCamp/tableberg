00fa7453da368da5404a619ae051036f9eab0e16
Saad <sakib.saad.khan@gmail.com>
2024-04-25 15:03:00 +0600

    fix: render both wp & fa icons

--- packages/components/icon-library/index.tsx
+++ packages/components/icon-library/index.tsx
@@ -41,7 +41,8 @@ export default IconsLibrary;
 export { default as IconPickerMini } from "./IconPickerMini";
 
 export function Icon({ icon, size, style }: any) {
-    const { viewBox, xmlns, children } = icon.icon.props;
+    
+    const { viewBox, xmlns, children } = icon.props || icon.icon.props;
     const pathData = children.props.d;
 
     return (
