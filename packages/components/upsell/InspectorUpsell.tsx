d38be34e1dc7a8295ee9f516fcfdd26360c9368e
Saad <sakib.saad.khan@gmail.com>
2024-04-27 16:40:32 +0600

    conditionally show notice

--- packages/components/upsell/InspectorUpsell.tsx
+++ packages/components/upsell/InspectorUpsell.tsx
@@ -1,7 +1,11 @@
 import { InspectorControls } from "@wordpress/block-editor";
 import { __ } from "@wordpress/i18n";
+import { isProActive } from "../../tableberg/src/utils";
 
 export default function InspectorUpsell() {
+    if (isProActive()) {
+        return null;
+    }
     const logoUrl = tablebergAdminMenuData?.assets.logo;
     return (
         <InspectorControls>
