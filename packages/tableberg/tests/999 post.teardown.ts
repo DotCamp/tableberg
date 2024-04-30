9ec90cdff5dd9db763617836c13551b03e654458
Zahin Azmayeen <zahin06@gmail.com>
2024-05-01 01:32:51 +0600

    remove playwright (will add later to the base repo)

--- packages/tableberg/tests/999 post.teardown.ts
+++
@@ -1,9 +0,0 @@
-import { test as setup } from "@playwright/test";
-import { POST_NAME } from "./vars";
-
-setup("delete test post", async ({ page }) => {
-    await page.goto("/wp-admin/edit.php");
-    await page.getByRole('cell', { name: '“playwright test” (Edit) —' }).hover();
-    await page.getByLabel(`Move “${POST_NAME}” to the Trash`).click({ force: true });
-});
-
