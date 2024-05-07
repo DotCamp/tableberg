e8abedc7fdddb83ad4ef3fd8ec94389e64edd929
Saad <sakib.saad.khan@gmail.com>
2024-05-07 11:30:45 +0600

    added all options for alignment

--- packages/components/editor/ToolbarWithDropdown.tsx
+++ packages/components/editor/ToolbarWithDropdown.tsx
@@ -1,5 +1,12 @@
-import { ToolbarGroup } from '@wordpress/components';
-import { alignNone, positionLeft, positionCenter, positionRight } from '@wordpress/icons';
+import { ToolbarGroup } from "@wordpress/components";
+import {
+    alignNone,
+    positionLeft,
+    positionCenter,
+    positionRight,
+    stretchFullWidth,
+    stretchWide,
+} from "@wordpress/icons";
 
 interface ToolbarGroupControlProps {
     icon?: JSX.Element;
@@ -9,13 +16,69 @@ interface ToolbarGroupControlProps {
 }
 
 interface AlignmentToolbarProps {
-    icon?: JSX.Element
+    icon?: JSX.Element;
     title: string;
-    onChange: (newAlign?: string) => void,
+    onChange: (newAlign?: string) => void;
     value: string | undefined;
     controls?: ToolbarGroupControlProps[];
-    controlset?: "alignment";
+    controlset?: "alignment" | "all";
 }
+
+const alignControls: ToolbarGroupControlProps[] = [
+    {
+        icon: alignNone,
+        title: "None",
+        value: undefined,
+    },
+    {
+        icon: positionLeft,
+        title: "Align left",
+        value: "left",
+    },
+    {
+        icon: positionCenter,
+        title: "Align center",
+        value: "center",
+    },
+    {
+        icon: positionRight,
+        title: "Align right",
+        value: "right",
+    },
+];
+
+const alignControlsWithWidth: ToolbarGroupControlProps[] = [
+    {
+        icon: alignNone,
+        title: "None",
+        value: undefined,
+    },
+    {
+        icon: positionLeft,
+        title: "Align left",
+        value: "left",
+    },
+    {
+        icon: positionCenter,
+        title: "Align center",
+        value: "center",
+    },
+    {
+        icon: positionRight,
+        title: "Align right",
+        value: "right",
+    },
+    {
+        icon: stretchWide,
+        title: "Wide width",
+        value: "wide",
+    },
+    {
+        icon: stretchFullWidth,
+        title: "Full Width",
+        value: "full",
+    },
+];
 
 export default function ToolbarWithDropdown({
     title,
@@ -24,50 +87,29 @@ export default function ToolbarWithDropdown({
     controls,
     controlset,
 }: AlignmentToolbarProps) {
-    const alignControls: ToolbarGroupControlProps[] = [
-        {
-            icon: alignNone,
-            title: "None",
-            value: undefined
-        },
-        {
-            icon: positionLeft,
-            title: "Align left",
-            value: "left"
-        },
-        {
-            icon: positionCenter,
-            title: "Align center",
-            value: "center"
-        },
-        {
-            icon: positionRight,
-            title: "Align right",
-            value: "right"
-        },
-    ];
-
     const controlsets: Record<string, ToolbarGroupControlProps[]> = {
-        "alignment": alignControls
-    }
+        alignment: alignControls,
+        all: alignControlsWithWidth,
+    };
 
     if (controlset) {
         controls = controlsets[controlset];
     }
 
     if (controls) {
-        return <ToolbarGroup
-            icon={controls.find((i) => i.value === value)!.icon}
-            title={title}
-            isCollapsed
-            controls={controls.map(control => {
-                return {
-                    ...control,
-                    onClick: () => onChange(control.value),
-                    isActive: value === control.value
-                }
-            })}
-        />
+        return (
+            <ToolbarGroup
+                icon={controls.find((i) => i.value === value)!.icon}
+                title={title}
+                isCollapsed
+                controls={controls.map((control) => {
+                    return {
+                        ...control,
+                        onClick: () => onChange(control.value),
+                        isActive: value === control.value,
+                    };
+                })}
+            />
+        );
     }
 }
-
