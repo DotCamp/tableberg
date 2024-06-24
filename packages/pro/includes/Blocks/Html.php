4681960958f5a346faf3dd7922417942d5250659
Saad <sakib.saad.khan@gmail.com>
2024-06-24 12:32:00 +0600

    use iframe to render custom-html-frontend

--- packages/pro/includes/Blocks/Html.php
+++ packages/pro/includes/Blocks/Html.php
@@ -3,6 +3,7 @@
 namespace Tableberg\Pro\Blocks;
 
 use Tableberg\Pro\Defaults;
+
 /**
  * Register HTML
  *
@@ -12,17 +13,19 @@ use Tableberg\Pro\Defaults;
 /**
  * Manage HTML registration.
  */
-class Html {
+class Html
+{
 
 	/**
 	 * Constructor
 	 *
 	 * @return void
 	 */
-	public function __construct() {
-		add_action( 'init', array( $this, 'block_registration' ) );
+	public function __construct()
+	{
+		add_action('init', array($this, 'block_registration'));
 	}
-	
+
 	/**
 	 * Renders the custom cell block on the server.
 	 *
@@ -31,22 +34,24 @@ class Html {
 	 * @param WP_Block $block      The block object.
 	 * @return string Returns the HTML content for the custom cell block.
 	 */
-	public function render_block( $attributes, $content, $block ) {
-		$html = isset( $attributes['content'] ) ? $attributes['content'] : '';
-		return preg_replace('#<script(.*?)>(.*?)</script>#is', '', $html);
+	public function render_block($attributes, $content, $block)
+	{
+		$html = isset($attributes['content']) ? $attributes['content'] : '';
+		return preg_replace('#<script(.*?)>(.*?)</script>#is', '', '<div class="tableberg-custom-html"></div><template>' . $html . '</template>');
 	}
 
 	/**
 	 * Register the block.
 	 */
-	public function block_registration() {
+	public function block_registration()
+	{
 		$defaults = new Defaults();
 
 		register_block_type_from_metadata(
 			TABLEBERG_PRO_DIR_PATH . 'dist/blocks/html/block.json',
 			array(
-				'attributes'      => $defaults->get_default_attributes( 'tableberg/html' ),
-				'render_callback' => array( $this, 'render_block' ),
+				'attributes' => $defaults->get_default_attributes('tableberg/html'),
+				'render_callback' => array($this, 'render_block'),
 			)
 		);
 	}
