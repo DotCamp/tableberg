e9259d7fe9b38b165022fa4594543e420990420b
Rattin99 <rattin99m18@gmail.com>
2024-09-28 15:57:01 +0600

    textdomain loaded on init

---
+++ packages/tableberg/languages/tableberg-de_CH.l10n.php
@@ -0,0 +1,2 @@
+<?php
+return ['project-id-version'=>'Tableberg','report-msgid-bugs-to'=>'','pot-creation-date'=>'2024-09-28 09:48+0000','po-revision-date'=>'2024-09-28 09:50+0000','last-translator'=>'','language-team'=>'German (Switzerland)','language'=>'de_CH','plural-forms'=>'nplurals=2; plural=n != 1;','mime-version'=>'1.0','content-type'=>'text/plain; charset=UTF-8','content-transfer-encoding'=>'8bit','x-generator'=>'Loco https://localise.biz/','x-loco-version'=>'2.6.11; wp-6.6.2','x-domain'=>'tableberg','messages'=>[]];
