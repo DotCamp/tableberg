2621b53c09b61d7f82b8bc1ef69d7071197c73c5
Rattin99 <rattin99m18@gmail.com>
2024-10-21 23:09:20 +0600

    added Tab.php to pro/Blocks

---
+++ packages/pro/includes/Blocks/Tab.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tableberg\Pro\Blocks;
+
+/**
+ *
+ *@package Tableberg_pro 
+ */
+
+class Tab
+{
+
+    public function __construct()
+    {
+        add_action("init", [$this, "register_block"]);
+    }
+
+    public function render_tab_block()
+    {
+        return "";
+    }
+
+    public function register_block()
+    {
+        $json = TABLEBERG_PRO_DIR_PATH . 'dist/blocks/tab/block.json';
+        $attrs = json_decode(file_get_contents($json), true)['attributes'];
+
+        register_block_type_from_metadata(
+            $json,
+            [
+                "attributes" => $attrs,
+                "render_callback" => [$this, "render_tab_block"]
+            ]
+        );
+    }
+}
