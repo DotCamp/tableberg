ba40e6c4dcdb6b00dcea2fbebf9932626cc36ee3
Rattin99 <rattin99m18@gmail.com>
2024-11-13 04:00:41 +0600

    implement active inactive background, hover, text color in frontend

--- packages/pro/includes/Blocks/Tab.php
+++ packages/pro/includes/Blocks/Tab.php
@@ -33,9 +33,21 @@ class Tab
     {
         $alignment_class = isset($attributes['alignment']) ? $attributes['alignment'] : 'left';
         $gap_style = isset($attributes['gap']) ? 'margin-bottom:' . $this->getSpacingCss($attributes['gap'])  : '';
+        $activeColor = isset($attributes['activeColor']) ? $attributes['activeColor'] : '#0000ff';
+        $inactiveColor = isset($attributes['inactiveColor']) ? $attributes['inactiveColor'] : '#cccccc';
+        $activeBackground = isset($attributes['activeBackground']) ? $attributes['activeBackground'] : '#ffffff';
+        $inactiveBackground = isset($attributes['inactiveBackground']) ? $attributes['inactiveBackground'] : '#f0f0f0';
+        $activeText = isset($attributes['activeText']) ? $attributes['activeText'] : '#ffffff';
+        $inactiveText = isset($attributes['inactiveText']) ? $attributes['inactiveText'] : '#333333';
 
 
-        $output = '<div class="tab-block">';
+        $output = '<div class="tab-block" style="
+        --tab-active-color: ' . esc_attr($activeColor) . ';
+        --tab-inactive-background-color: ' . esc_attr($inactiveBackground) . ';
+        --tab-active-background-color: ' . esc_attr($activeBackground) . ';
+        --tab-active-text-color: ' . esc_attr($activeText) . ';
+        --tab-inactive-text-color: ' . esc_attr($inactiveText) . ';
+        --tab-inactive-color: ' . esc_attr($inactiveColor)  . ';">';
 
         $output .= '<nav class="tab-headings ' . esc_attr($alignment_class) . '" style="' . $gap_style . '">';
 
