55553d640e32dfcd132b99430fc6c81704dc0bc9
Erdem Bircan <erdem.erdembircan@gmail.com>
2024-11-12 14:32:47 +0300

    documentation update for InView component

--- packages/tableberg/src/components/InView.tsx
+++ packages/tableberg/src/components/InView.tsx
@@ -1,15 +1,24 @@
-import {useEffect, useRef, useState} from "react";
+import React, {useEffect, useRef, useState} from "react";
 
 interface InViewProps {
     children: React.ReactNode;
     viewRatio?: number;
 }
 
+/**
+ * Component to render children when they are in view.
+ *
+ * @param children Component to render when in view.
+ * @param viewRatio Ratio of the element that needs to be in view to trigger the render. (0.1 - 1.0)
+ *
+ * @constructor
+ */
 export default function InView({children, viewRatio = 1.0}: InViewProps) {
     const [isInView, setIsInView] = useState(false);
 
     const wrapperRef = useRef(null);
 
+    // component observer initialization
     const observer = new IntersectionObserver(([entry]) => {
         const {isIntersecting} = entry;
 
